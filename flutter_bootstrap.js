998ea024e86bb5474ad0093f51144a7aa4a11bec
onionwind98 <samuelchai1998@hotmail.com>
2025-01-18 11:01:20 +0800

    180115.v1 build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1927771394"
+    serviceWorkerVersion: "1497438894"
   }
 });
